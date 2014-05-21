0c364960b5eb37e05bcec171060e2203dda4076a
Martin N <mr.avgp@googlemail.com>
2014-05-21 20:06:41 +0000

    Ohai firefox OS

--- js/controllers.js
+++ js/controllers.js
@@ -3,3 +3,39 @@ function AddDocCtrl($scope) {
     alert($scope.name);
   }
 }
+
+function SettingsCtrl($scope) {
+  $scope.installed = false;
+  $scope.installable = !!(window.navigator.mozApps);
+  if(window.navigator.mozApps) {
+    var request = window.navigator.mozApps.getSelf();
+    request.onsuccess = function() {
+      if (request.result) {
+        $scope.$apply(function() {
+          $scope.installed = true; // already installed
+        });
+      } else {
+        $scope.$apply(function() {
+          $scope.installed = false;  // not installed yet
+        });
+      }
+    }
+  }
+  
+  $scope.install = function() {
+    if(!window.navigator.mozApps) {
+      alert("Whoops, your platform does not support app installation from the web!");
+      return;
+    }
+    var install = window.navigator.mozApps.install('http://avgp.github.io/pencraft/manifest.webapp');
+    install.onerror = function() {
+      alert("Installation has failed: " + this.error.name);
+    }
+    install.onsuccess = function() {
+      alert("Successfully installed!");
+      $scope.$apply(function() {
+        $scope.installed = true;
+      })
+    }
+  }
+}
